1fe90d2f6994c4974e7cc2120b0c9db3c9cfabfd
Spectre <spectre_hidn@protonmail.com>
2022-02-03 02:29:16 +0530

    Add files via upload
    Initial commit

---
+++ AMD_Processor-Fastboot.Fix.bat
@@ -0,0 +1,56 @@
+@echo off
+
+echo [38;5;81mRegistry Hack to detect Xiaomi devices in fastboot mode on AMD series processors.[0m
+echo Make sure latest stable ADB and fastboot drivers are already installed. (Get it from here --^> https://shorturl.at/yHLW5)
+echo [38;5;190mAdministravite Permissions required to modify registry![0m
+echo[
+echo [PRESS ENTER TO START]
+pause >nul 2>&1
+if [%1] == [--skip-backup] (
+	GOTO Add_reg
+) else (
+	GOTO backup_hklm
+)
+
+:Add_reg
+	
+	echo[
+	echo [38;5;135mApplying Reg hacks...[0m
+	timeout 2 >nul 2>&1
+	reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "osvc" /t REG_BINARY /d "0000" /f >nul 2>&1
+	if %ERRORLEVEL% == 0 (
+		echo [38;5;82mSucessfully added 033[38;5;162m'osvc'033[38;5;82m Registry key![0m
+	) else (
+		echo [38;5;196mError occured while adding 'osvc' key! - Already present or Lack of permissions.[0m
+	)
+	reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "SkipContainerIdQuery" /t REG_BINARY /d "01000000" /f >nul 2>&1
+	if %ERRORLEVEL% == 0 (
+		echo [38;5;82mSucessfully added [38;5;162m'SkipContainerIdQuery'[38;5;82m Registry key![0m
+	) else (
+		echo [38;5;196mError occured while adding 'SkipContainerIdQuery' key! - Already present or Lack of permissions.[0m
+	)
+	reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "SkipBOSDescriptorQuery" /t REG_BINARY /d "01000000" /f >nul 2>&1
+	if %ERRORLEVEL% == 0 (
+		echo [38;5;82mSucessfully added [38;5;162m'SkipBOSDescriptorQuery'[38;5;82m Registry key![0m
+	) else (
+		echo [38;5;196mError occured while adding 'SkipBOSDescriptorQuery' key! - Already present or Lack of permissions.[0m
+	)
+	pause >nul 2>&1
+	goto EOF
+
+:backup_hklm
+	echo[
+	echo [38;5;135mBacking up usbflags registry key...[0m
+	timeout 2 >nul 2>&1
+	reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\ %~dp0\usbflags_backup.reg > nul
+	if %errorlevel% == 0 (
+		goto Add_reg
+	) else (
+		echo [38;5;208mError occured during backing up registry keys![0m
+		echo [Press Enter] to continue patching keys without backing up else close the terminal.
+		pause >nul 2>&1
+		goto Add_reg
+	)
+
+:EOF
+echo[
---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+<h1>Fastboot Fix for AMD Processors</h1>
+<strong><i>A bloat-less script to hack registry keys to detect xiaomi devices under fastboot mode on notorious AMD Processors.</i></strong>
+<br>
+<br>
+</div>
+
+AMD Processors can't handle fastboot mode on Android devices having qualcomm chipset especially, on Xiaomi devices. This batch script will add certain registry keys to workaround with that problem (until AMD and Xiaomi roll out official patches for the drivers).
+
+>I am not the one who discover this fix. An unknown developer from the great **Custom Rom** community is the one behind this magic. I had just automate this process for *newbies.*
+
+### Usage
+- Downlaod the batch script from the release tab or clone this repo.
+- Right click the batch script and click on `Run As Administrator` to start the script
+- Press `[Enter]` to start patching registry keys... 
+
+Additionally, you can pass `--skip-backup` flag to bypass registry backup process.
+- Open Terminal/cmd on the same directory (Type `cmd` on the address bar of the file explorer)
+- Execute `.\AMD_Processor-Fastboot.Fix.bat --skip-backup` to run script without backing up Reg keys.
+
+## What it fixes?
+- Fastboot.exe: `???????????????` on connected device list.
+-> In some cases, this happens on executing `fastboot devices` command second time.
+
+- "Press any key to shutdown" showing at the top-left corner of the screen while in fastboot mode.
+- Mi Unlock tool: Device not connected!
+- Fastboot.exe: Failed to download errors
+- Fastboot.exe: Waiting for the device...
+
+Literally, Anything related to fastboot.
+
+## Share & Star
+If this script works for you then spare some seconds to share this to the needy and give it a star 🌟to make this reachable to other people...
